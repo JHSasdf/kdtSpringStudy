d34b844d10e5297bc15da0a1ec7618c9992db3ae
선지훈 <sgh4760@naver.com>
2024-03-07 00:40:45 +0900

    study:240307\

--- codingon-spring-boot/src/main/java/codingon/codingonspringboot/controller/_01_thymeleaf/jwt/JwtAuthSecurityConfiguration.java
+++ codingon-spring-boot/src/main/java/codingon/codingonspringboot/controller/_01_thymeleaf/jwt/JwtAuthSecurityConfiguration.java
@@ -1,61 +1,127 @@
-package codingon.codingonspringboot.controller._01_thymeleaf.basic;
+package codingon.codingonspringboot.controller._01_thymeleaf.jwt;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 import javax.sql.DataSource;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+import java.util.UUID;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-//@Configuration
-public class BasicAuthSecurityConfiguration {
+@Configuration
+public class JwtAuthSecurityConfiguration {
 
+    // 이 secutiryFilterchain이 요청을 인증하게 된다.
     @Bean
     SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
         // HTTP 요청에 대한 권한 부여 적용
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-        
+
         // HTTP 세션에 사용할 정책을 STATELESS로 설정하기 (REST API에서 설정해야 함.)
         // 스프링 부트 기본 옵션에서는 세션을 이용해서 로그인 로그아웃을 설정함.
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        // 폼 로그인 사용할 건지
-//        http.formLogin(withDefaults());
-        
         // 기본 인증 사용
         http.httpBasic(withDefaults());
 
         // csrf 사용 해제
         http.csrf(AbstractHttpConfigurer::disable);
         http.headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+//        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        // jwt
+        http.oauth2ResourceServer((oauth2) -> oauth2
+                .jwt(Customizer.withDefaults())
+        );
         return http.build();
     }
 
-    // UserDetailsService : 사용자별 데이터를 로드하는 코어 인터페이스
-    // in memory user details manager
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        // 메모리에 저장
-//        var user = User.withUsername("in28minutes").password("{noop}dummy").roles("USER").build();
-//        var admin = User.withUsername("admin").password("{noop}dummy").roles("ADMIN").build();
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
+
+
+    // 1. key pair 만들기
+    @Bean
+    public KeyPair keyPair() {
+        try {
+        var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    // 2. key pair 이용해서 RSA key Object 만들기
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair) {
+        return new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+                .privateKey(keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    // 3. JWKSource 만들기 (JSON Web Key source)
+    // / Create JWKSet (a new JSON Web Key Set) with the RSA Key
+    // / Create JWKSource using the JWKSet
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+        var jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, context) -> jwkSelector.select(jwkSet);
+//        var jwkSource =
+//                new JWKSource() {
+//
+//            @Override
+//            public List<JWK> get(JWKSelector jwkSelector, SecurityContext context) throws KeySourceException {
+//                return jwkSelector.select(jwkSet);
+//            }
+//        };
+    }
+
+// 4. User RSa Public Key for Decoding JWTDecoder 만들기
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey()).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
+
+
+
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
@@ -87,4 +153,5 @@ public class BasicAuthSecurityConfiguration {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
     }
+
 }
---
+++ codingon-spring-boot/src/main/java/codingon/codingonspringboot/controller/_01_thymeleaf/jwt/JwtAuthenticationResource.java
@@ -0,0 +1,56 @@
+package codingon.codingonspringboot.controller._01_thymeleaf.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@RestController
+public class JwtAuthenticationResource {
+
+    // 1. 기본 인증 사용해서 JWT 토큰 받기
+    // 2-n. REST API에 대한 요청 인증하기 위해서 JWT 토큰을 Bearer 토큰으로 활용
+
+
+    private JwtEncoder jwtEncoder;
+
+    @Autowired
+    public JwtAuthenticationResource(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(Authentication authentication) {
+        return new JwtResponse(createToken(authentication));
+    }
+
+    private String createToken(Authentication authentication) {
+        // claims 생성
+        var claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plusSeconds(60 * 30))
+                .subject(authentication.getName())
+                .claim("scope", createScope(authentication)) // authority
+                .build();
+
+        // jwtEncoderParameters 생성하고 이걸 인코딩하고 TokenValue 추출해서 return
+        JwtEncoderParameters parameters = JwtEncoderParameters.from(claims);
+        return jwtEncoder.encode(parameters).getTokenValue();
+    }
+
+    private Object createScope(Authentication authentication) {
+        // 권한 받기
+        return authentication.getAuthorities().stream() // list of Authority objects
+                .map(a -> a.getAuthority()) // list of String
+                .collect(Collectors.joining(" "));
+    }
+}
+
+record JwtResponse(String token) {}
