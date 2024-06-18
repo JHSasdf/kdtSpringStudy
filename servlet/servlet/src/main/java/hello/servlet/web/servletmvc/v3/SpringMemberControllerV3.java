d72d97f19d628b486bbf2449b976cb62306c68bd
선지훈 <sgh4760@naver.com>
2024-06-18 21:06:23 +0900

    study:240618

---
+++ servlet/servlet/src/main/java/hello/servlet/web/servletmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,41 @@
+package hello.servlet.web.servletmvc.v3;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/save")
+    public String process1(@RequestParam("username") String username,
+                           @RequestParam("age") int age,
+                           Model model) {
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+        model.addAttribute("member", member);
+        return "save-result";
+
+    }
+
+    @RequestMapping("/new-form")
+    public String process2() {
+        return "new-form";
+    }
+
+    @RequestMapping("")
+    public String process3(Model model) {
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members";
+    }
+}
